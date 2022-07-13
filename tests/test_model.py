e61cb5c447d02ec16f6921ce5bc05576d6f27a10
Nguyen Thomas <thomas.nguyen@bedrockstreaming.com>
2022-07-13 10:41:57 +0200

    fix tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,11 +4,11 @@ model = load_model()
 
 
 def test_coherency_results():
-    x = [["100", "3", "1"]]
+    x = [[100, 3, 1]]
     assert(model.predict(x)[0] > 10000)
 
-    x = [["100", "3", "-10"]]
+    x = [[100, 3, -10]]
     assert(model.predict(x)[0] < 0)
 
-    x = [["10000", "20", "1"]]
+    x = [[10000, 20, 1]]
     assert(model.predict(x)[0] > 1000000)
